1885d363f5b9e385ba67a6d94fc5d9e362b120b1
Marina <marishka_vrn@list.ru>
2020-08-14 18:21:52 +0300

    All tests passed

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -8,12 +8,13 @@ public class SimpleBigNumbersService implements BigNumbersService {
     /**
      * Метод делит первое число на второе с заданной точностью
      * Например 1/3 с точностью 2 = 0.33
+     *
      * @param range точность
      * @return результат
      */
     @Override
     public BigDecimal getPrecisionNumber(int a, int b, int range) {
-        return null;
+        return new BigDecimal(a).divide(new BigDecimal(b), range, 5);
     }
 
     /**
@@ -24,6 +25,21 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        int count = range;
+        long result = 2;
+        while (count >= 0) {
+            boolean simple = true;
+            for (int i = 2; i < result; i++) {
+                if (result % i == 0) {
+                    simple = false;
+                    break;
+                }
+            }
+            if (simple) {
+                count--;
+            }
+            result++;
+        }
+        return BigInteger.valueOf(result - 1);
     }
 }
